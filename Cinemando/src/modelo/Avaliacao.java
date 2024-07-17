cb7b73622f0c0a4be7b6ef0881d096d676c2b322
JeovanaGSilva <142043097+JeovanaGSilva@users.noreply.github.com>
2024-07-17 10:09:21 -0300

    Avaliacao.java

--- Cinemando/src/modelo/Avaliacao.java
+++ Cinemando/src/modelo/Avaliacao.java
@@ -1,68 +1,58 @@
-package domain;
-
-import java.util.Date;
-
+package modelo;
+import modelo.Usuario;
+import modelo.Filme;
 public class Avaliacao {
 
-    private Integer cod_avaliacao;
-    private Integer nota;
-    private Date data_avaliacao; 
-    private Filme filme; 
-    private Usuario usuario;
+    private Integer avaliCod;
+    private Integer avaliNota;
+    private String avaliData;
+    private Usuario usuario; 
+    private Filme filme;
+      
+    public Avaliacao() {
+    }
     
-    public Avaliacao(Integer cod_avaliacao, Integer nota, Date data_avaliacao, Filme filme, Usuario usuario) {
-        this.cod_avaliacao = cod_avaliacao;
-        this.nota = nota;
-        this.data_avaliacao = data_avaliacao;
-        this.filme = filme;
-        this.usuario = usuario;
+    public Integer getAvaliCod() {
+        return avaliCod;
     }
-
-    public Integer getCod_avaliacao() {
-        return cod_avaliacao;
+    public void setAvaliCod(Integer avaliCod) {
+        this.avaliCod = avaliCod;
     }
-
-    public void setCod_avaliacao(Integer cod_avaliacao) {
-        this.cod_avaliacao = cod_avaliacao;
+    public Integer getAvaliNota() {
+        return avaliNota;
     }
-
-    public Integer getNota() {
-        return nota;
+    public void setAvaliNota(Integer avaliNota) {
+        this.avaliNota = avaliNota;
     }
-
-    public void setNota(Integer nota) {
-        this.nota = nota;
+    public String getAvaliData() {
+        return avaliData;
     }
-
-    public Date getData_avaliacao() {
-        return data_avaliacao;
+    public void setAvaliData(String avaliData) {
+        this.avaliData = avaliData;
     }
-
-    public void setData_avaliacao(Date data_avaliacao) {
-        this.data_avaliacao = data_avaliacao;
-    }
-
-    public Filme getFilme() {
-        return filme;
-    }
-
-    public void setFilme(Filme filme) {
-        this.filme = filme;
-    }
-
-    public Usuario getUsuario() {
+     public Usuario getUsuario() {
         return usuario;
     }
-
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
+    
+    public Filme getfilme() {
+        return filme;
+    }
+    public void setfilme(Filme filme) {
+        this.filme = filme;
+    }
+    
+    public Avaliacao(Integer avaliCod, Integer avaliNota, String avaliData, Usuario usuario, Filme filme) {
+        this.avaliCod = avaliCod;
+        this.avaliNota = avaliNota;
+        this.avaliData = avaliData;
+        this.usuario = usuario;
+        this.filme = filme;
+    }
 
-    @Override
-    public String toString() {
-        return "Avaliacao [cod_avaliacao=" + cod_avaliacao + ", nota=" + nota + ", data_avaliacao=" + data_avaliacao
-                + ", filme=" + filme + ", usuario=" + usuario + "]";
-    } 
-
- 
+   
+    
+    
 }
