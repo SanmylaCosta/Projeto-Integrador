c060bd1cab2ca0c0f00f310a3e4ccd28dc511d77
JeovanaGSilva <142043097+JeovanaGSilva@users.noreply.github.com>
2024-07-17 10:08:10 -0300

    Diretor.java

--- Cinemando/src/modelo/Diretor.java
+++ Cinemando/src/modelo/Diretor.java
@@ -1,37 +1,40 @@
-package domain;
-
-import java.util.Date;
+package modelo;
 
 public class Diretor {
 
+    private Integer dirCod;
+    private String dirNome;
+    private String dirDataN;
 
-  private Integer cod_diretor;
-    private String nome_diretor;
-    private Date data_N_D;  
+    public Diretor(Integer dirCod, String dirNome, String dirDataN) {
+        this.dirCod = dirCod;
+        this.dirNome = dirNome;
+        this.dirDataN = dirDataN;
+    }
 
-    public Integer getCod_diretor() {
-        return cod_diretor;
-    }
-    public void setCod_diretor(Integer cod_diretor) {
-        this.cod_diretor = cod_diretor;
-    }
-    public String getNome_diretor() {
-        return nome_diretor;
-    }
-    public void setNome_diretor(String nome_diretor) {
-        this.nome_diretor = nome_diretor;
-    }
-    public Date getData_N_D() {
-        return data_N_D;
-    }
-    public void setData_N_D(Date data_N_D) {
-        this.data_N_D = data_N_D;
-    }
-    @Override
-    public String toString() {
-        return "Diretor [cod_diretor=" + cod_diretor + ", nome_diretor=" + nome_diretor + ", data_N_D=" + data_N_D
-                + "]";
+    public Diretor() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     
+
+    public Integer getDirCod() {
+        return dirCod;
+    }
+    public void setDirCod(Integer dirCod) {
+        this.dirCod = dirCod;
+    }
+    public String getDirNome() {
+        return dirNome;
+    }
+    public void setDirNome(String dirNome) {
+        this.dirNome = dirNome;
+    }
+    public String getDirDataN() {
+        return dirDataN;
+    }
+    public void setDirDataN(String dirDataN) {
+        this.dirDataN = dirDataN;
+    
 }
+    }
